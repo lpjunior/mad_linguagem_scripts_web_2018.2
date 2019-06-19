fb91a37c879d9b88821b771d37d0a13fc5eead48
Luis Paulo Junior <prof.lpjunior@gmail.com>
2019-06-19 12:08:44 -0300

    angular pic

--- Angular/senacpic/src/app/app.component.ts
+++ Angular/senacpic/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'senacpic';
+  titulo1 = 'Angular Wallpaper';
+  descricao1 = 'Foto retirada de uma equipe de desenvolvimento de Web da tecnologia Angular.';
+  seguir1 = "checked";
+  
+  titulo2 = 'MEAN Wallpaper';
+  descricao2 = 'Conceito de desenvolvimento web.';
 }
